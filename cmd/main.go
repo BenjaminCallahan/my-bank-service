80d9f44998ba9801578c0f5bffb3e2382904f9a1
Benjamin Callahan <>
2021-08-28 01:46:38 +0300

    added graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/BenjaminCallahan/my-bank-service/internal/api"
@@ -14,7 +22,26 @@ func main() {
 	handlers := api.NewHandler()
 
 	srv := api.NewServer("", handlers.InitRoutes())
-	if err := srv.Run(); err != nil {
-		logrus.Fatalf("error occurred while running server: %s\n", err.Error())
+	go func() {
+		if err := srv.Run(); !errors.Is(err, http.ErrServerClosed) {
+			logrus.Fatalf("error occurred while running server: %s\n", err.Error())
+		}
+	}()
+
+	logrus.Println("application started")
+
+	// graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	ctx, shutdown := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdown()
+
+	logrus.Println("application shutdown")
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logrus.Printf("error occurred while shutting down http server: %s\n", err)
 	}
 }
