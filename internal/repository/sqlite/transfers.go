89a9732fc31f602ca796f7735d5ec20e9bddeccd
Benjamin Callahan <>
2022-01-06 22:38:17 +0300

    Added method of processed transfers in sqlite

--- internal/repository/sqlite/transfers.go
+++ internal/repository/sqlite/transfers.go
@@ -21,3 +21,38 @@ func (q *Queries) CreateTransfer(amount float64) error {
 	return nil
 }
 
+// commitSuccesTransfer mark transfer as a Success
+func (q *Queries) commitSuccesTransfer() error {
+	_, err := q.dbTx.Exec("UPDATE transfers SET is_processed = ? WHERE to_account_id == ?", transferCompleted, accountID)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+// getNotProcessedTransfers get all not processed transfer on the current time
+func (q *Queries) getNotProcessedTransfers() ([]domain.Transfer, error) {
+	rows, err := q.dbTx.Query("SELECT * FROM transfers WHERE is_processed == 0")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var transfers []domain.Transfer
+	for rows.Next() {
+		var transfer domain.Transfer
+		if err = rows.Scan(
+			&transfer.ID,
+			&transfer.ToAccountID,
+			&transfer.Amount,
+			&transfer.IsProcessed,
+		); err != nil {
+			return nil, err
+		}
+		transfers = append(transfers, transfer)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return transfers, err
+}
